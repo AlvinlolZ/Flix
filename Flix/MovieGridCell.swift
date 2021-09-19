7a21f52829f9148ea1a8659d8f96d4baf7bc603f
Juntao Feng <79394873+AlvinlolZ@users.noreply.github.com>
2021-09-19 14:48:31 -0400

    Flix

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Alvin Feng on 9/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
